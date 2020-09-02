3c36a1accb1b7095e2c272a2011258a33b7bc782
dunapanta <dunapanta2@gmail.com>
2020-09-02 18:57:12 -0500

    Extraigo logica useContext en un custom hook

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,10 +1,10 @@
-import React, {useContext} from 'react'
-import GifsContext from '../../context/GifsContext'
+import React from 'react'
 import Gif from '../../components/Gif/Gif'
+import useGlobalGifs from '../../hooks/useGlobalGifs'
 
 export default function Detail({params}){
     //con context ya puedo leer y/o actualizar el estado desde aqui
-    const {gifs} = useContext(GifsContext)
+    const gifs = useGlobalGifs()
     console.log(gifs)
 
     const gif = gifs.find(singleGif => singleGif.id  === params.id)
