a5edcf89b79d917620203a29cd9556673f5406ac
Daniel Unapanta <dunapanta2@gmail.com>
2020-10-20 11:19:20 -0500

    Buscador en resultados

--- src/pages/SearchResults/SearchResults.js
+++ src/pages/SearchResults/SearchResults.js
@@ -1,6 +1,7 @@
 import React, {useCallback, useRef, useEffect} from 'react'
 import Spinner from '../../components/Spinner/Spinner'
 import ListOfGifs from '../../components/ListOfGifs/ListOfGifs'
+/* import SearchForm from '../../components/SearchForm/SearchForm' */
 import useGifs from '../../hooks/useGifs'
 import useNearScreen from '../../hooks/useNearScreen'
 import debounce from 'just-debounce-it'
@@ -39,6 +40,9 @@ export default function SearchResults({params}){
                     <Helmet>
                         <title>{title}</title>
                     </Helmet>
+                    <header className="o-header">
+                        <SearchForm />
+                    </header>
                     <div className="App-wrapper">
                         <h3 className="App-title">{decodeURI(keyword)}</h3>
                         <ListOfGifs gifs={gifs} />
